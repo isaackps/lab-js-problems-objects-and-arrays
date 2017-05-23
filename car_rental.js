05c8a5a305d2411948c17f7c87f3c8a0e19cf25a
Isaac Koh <isaackoh@Isaacs-MacBook-Pro.local>
2017-05-23 10:24:36 +0800

    car rented.js done but got error

--- car_rental.js
+++ car_rental.js
@@ -1,11 +1,43 @@
 // Customer Object
 var Customer = function (customerInfo) {
   // ** your code here**
+  this.id = customerInfo.id;
+  this.name = customerInfo.name;
+  this.carRented = null;
 };
 
 // Car Object
 var Car = function (carInfo) {
   // ** your code here**
+  this.id = carInfo.id;
+  this.producer = carInfo.producer;
+  this.model = carInfo.model;
+  this.rentalPricePerDay = carInfo.rentalPrice;
+  this.available = true;
+  this.customer = null;
+  this.rentalDuration = 0;
+  this.quotePrice = function(rentalDuration) {
+    return rentalDuration * this.rentalPrice;
+  }
+  this.reserve = function(customer, rentalDuration) {
+    if (this.available){
+     this.available = false;
+     this.customer = Customer.name;
+    this.rentalDuration = rentalDuration;
+      return true;
+    }else {
+      return false;
+    }
+  }
+  this.carReturn = function () {
+    if(this.available === true){
+      return "Sorry, this car have alredy been returned.";
+    }else{
+      this.available = true;
+      this.customer = null;
+      this.rentalDuration = 0;
+    }
+  }
 };
 
 // Vendor Object
@@ -38,8 +70,75 @@ var Vendor = function(name) {
     });
   };
 
-  // **your code here**
+  this.addCar = function (car) {
+    if (this.getCar(car.id)){
+      console.log("ID already exists");
+    } else {
+      this.cars.push(car);
+      console.log("Car added to warehouse");
+    }
+  }
+  this.addCustomer = function (customer) {
+    if (this.getCustomer(customer.id)){
+      console.log("ID already exists");
+    } else {
+      this.customers.push(customer);
+      console.log("Customer added to warehouse");
+    }
+  }
+  this.removeCar = function (car){
+    if (this.findCarIndex(car.id) >= 0) {
+      this.cars.splice(this.findCarIndex(car.id), this.findCarIndex(car.id)+1);
+      console.log("Car deleted");
+    }else {
+      console.log("Car not found");
+    }
+  }
+ this.removeCustomer = function (customer){
+    if (this.findCustomerIndex(customer.id) >= 0) {
+      this.customers.splice(this.findCustomerIndex(customer.id), this.findCustomerIndex(customer.id)+1);
+      console.log("Customer deleted");
+    }else {
+      console.log("Customer not found");
+    }
+  }
+this.availableCars = function () {
+  return this.cars.filter(function(car) {
+    return car.available ? true : false ;
+    });
+  }
+
+this.rentCar = function (customerID, rentalDuration) {
+  console.log(this.availableCars.length);
+  if (this.availableCars.length === 0) {
+    console.log("All our cars have been rented");
+  }else if (this.getCustomer === true) {
+    this.Customer.carRented = this.availableCars[0];
+    this.car.reserve(customerID, rentalDuration);
+    console.log("The car has been reserved");
+  }else {
+    console.log("Please provide a valid customerID");
+  }
+}
+
+this.returnCar = function (customerID) {
+  if (this.getCustomer) {
+    this.Customer.carRented = null;
+    this.Car.carReturn;
+    console.log("The car has been reserved");
+  } else {
+    console.log("Please provide a valid customerID");
+  }
+}
+
+this.totalRevenue = function () {
+  return this.cars.reduce(function (acc, val, ind, arr) {
+    return acc += val.quotePrice();
+  }, 0);
+}
+
 };
+
 
 
 // Codes you can run to test your code
@@ -56,13 +155,21 @@ var carInfo = {
   rentalPrice: 200,
 };
 
+var carInfo2 = {
+  id: "002",
+  producer: "Toyota",
+  model: "Subra",
+  rentalPrice: 200,
+};
 var carA = new Car(carInfo);
+var carB = new Car(carInfo2);
 
 var vendor = new Vendor('Jens Limited');
-vendor.addCustormer(customerA);
+vendor.addCustomer(customerA);
 
-console.log(vendor.availableCars());
+//console.log(vendor.availableCars());
 vendor.addCar(carA);
-console.log(vendor.availableCars());
+vendor.addCar(carB);
+//console.log(vendor.availableCars());
 
 vendor.rentCar(customerA.id, 5);
